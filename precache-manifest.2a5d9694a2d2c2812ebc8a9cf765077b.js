e02a573c8a49513967666abdafa20c76516ce6dc
abubakar sambo <abubakarsambo@abubakars-MacBook-Pro.local>
2020-06-25 12:38:26 +0100

    Updates

---
+++ precache-manifest.2a5d9694a2d2c2812ebc8a9cf765077b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07f481b16e448f18addac7bf832c20a",
+    "url": "/poc/index.html"
+  },
+  {
+    "revision": "6d5436fe869cdb4b0d67",
+    "url": "/poc/static/css/main.69cf9b05.chunk.css"
+  },
+  {
+    "revision": "bdbd260ea89548b918af",
+    "url": "/poc/static/js/2.5fe6ff55.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/poc/static/js/2.5fe6ff55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5436fe869cdb4b0d67",
+    "url": "/poc/static/js/main.0e4e8694.chunk.js"
+  },
+  {
+    "revision": "ff0b2a4cc03eeb76053d",
+    "url": "/poc/static/js/runtime-main.dbc7cb38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
